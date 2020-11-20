de09e4d4d87579496461e0a76f964f5f85c2b3f8
MaelDelaby <maeldelabouy@yahoo.fr>
2020-11-20 10:09:08 +0100

    #2 ajout route delete

--- Services/graphWriter/routes/graphWriter.js
+++ Services/graphWriter/routes/graphWriter.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const graphWriterRouter = express.Router();
 const graphWriterController = require('../controllers/graphWriter');
-const { body, validationResult } = require('express-validator');
+const { body, query, validationResult } = require('express-validator');
 
 graphWriterRouter.post('/graph', [body('type').matches(
    "histogramme"
-)]
-   , (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-         return res.status(400).json({ errors: errors.array() });
-      }
+)], (req, res, next) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+   }
 
-      const graphId = graphWriterController.graphCreation(req.body.type);
+   const graphId = graphWriterController.graphCreation(req.body.type);
 
-      res.status(201).send(graphId);
+   res.status(201).send(graphId);
 
-      next();
-   });
+   next();
+});
+
+graphWriterRouter.delete('/graph', [query('id').exists()], (req, res, next) => {
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+   }
+
+   graphWriterController.graphDelete(req.query.id);
+
+   res.status(201).send("DELETED");
+
+   next();
+});
 
 
 module.exports = graphWriterRouter;
