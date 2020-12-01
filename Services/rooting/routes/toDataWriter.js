8cf0724182c4257a0eec802d517c74e1c7f76069
MaelDelaby <maeldelabouy@yahoo.fr>
2020-12-01 15:38:50 +0100

    #35 ajout route timeSeries

--- Services/rooting/routes/toDataWriter.js
+++ Services/rooting/routes/toDataWriter.js
@@ -3,42 +3,29 @@ const axios = require('axios').default;
 const dataWriterRouter = express.Router();
 const { body,param, validationResult } = require('express-validator');
 
-dataWriterRouter.post('/datawriter', [body("points").isArray()]
-   , async(req, res, next) => {
-
-   const errors = validationResult(req);
-   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-   }
+writeTimeSeriesDataRouter.get('/datas/timeseries', async (req, res, next) => {
 
    try {
-      const reponse = await axios.post(process.env.DATAWRITER_ADDR + "/datawriter", req.body )
-      
-
-      res.status(200).send(reponse.data.toString());
-   } catch (error) {      
-      res.status(501).send(error);
+      const reponse = await axios.get(process.env.DATAWRITER_ADDR + "/datawriter/timeseries")
+      return res.status(201).send(reponse.data)
+   } catch (error) {
+      return res.status(501).send(error)
    }
-
-   next();
 });
 
-dataWriterRouter.delete('/datawriter/:id', [param('id').isInt()], async (req, res, next) => {
-   
+writeTimeSeriesDataRouter.post('/datas/timeseries/:id',[param("id").isString(),body("timestamp").isInt(),body("data").isInt()], async (req, res, next) => {
+
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+       return res.status(400).json({ errors: errors.array() });
    }
-  
+   
    try {
-      const reponse = await axios.delete(process.env.DATAWRITER_ADDR + "/datawriter/" + req.params.id)
-      
-      res.status(200).send(reponse.data.toString());
-   } catch (error) {      
-      res.status(501).send(error);
+      const reponse = await axios.get(process.env.DATAWRITER_ADDR + "/datawriter/timeseries", {"id" : req.param.id, "timestamp" : req.body.timestamp, "value" : req.body.data})
+      return res.status(201).send(reponse.data)
+   } catch (error) {
+      return res.status(501).send(error)
    }
-
-   next();
 });
 
 
