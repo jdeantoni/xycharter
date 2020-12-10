0b0e71aa1ae8f39ab8bade7c10109b48f891c472
MaelDelaby <maeldelabouy@yahoo.fr>
2020-12-10 11:44:15 +0100

    #75 donut with multiple dataset

--- Services/quickChartRender/routes/render.js
+++ Services/quickChartRender/routes/render.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const renderRouter = express.Router();
 const { param, query, validationResult } = require('express-validator');
-const { renderFromQC } = require('../controllers/render');
+const doughnutRender = require('../controllers/doughnut');
+const axios = require('axios').default;
 
 renderRouter.get('/graphs/:id', [query('type').matches(
    "PNG|JPG"
@@ -13,7 +14,17 @@ renderRouter.get('/graphs/:id', [query('type').matches(
       return res.status(400).json({ errors: errors.array() });
    }
 
-   const reponse = await renderFromQC(req.params.id, req.query.type);
+
+   const graphCharas = await (await axios.get(process.env.DBREADER_ADDR + "/graphs/" + req.params.id)).data[0];
+
+   var reponse;
+
+   switch (graphCharas.graphtype){
+      case "doughnut":
+         reponse = await doughnutRender.renderFromQC(req.params.id, graphCharas.characteristics);
+         break;
+   }
+   
 
    res.setHeader("Content-Type", "image/jpg");
    res.status(201).send(reponse);
